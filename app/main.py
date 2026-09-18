83a4d9f657e648cf0fc8d39fffe76e379a6f547a
jogarobi <rodriguezbidojosegabriel@gmail.com>
2026-09-17 20:06:31 -0400

    feat: add file reading and parsing

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+import csv
+from io import StringIO
+
+from fastapi import FastAPI, HTTPException, UploadFile
+
+from app.utils import increase_csv_field_size_limit
 
 app = FastAPI()
+
+increase_csv_field_size_limit()
 
 
 @app.get("/")
@@ -11,3 +18,28 @@ async def read_root():
 @app.get("/message/{id}")
 async def read_item(id):
     return {"message": f"Your message ID is {id}"}
+
+
+@app.post("/dataset/ingest")
+async def read_dataset(file: UploadFile):
+    if file.content_type != "text/csv":
+        raise HTTPException(status_code=400, detail="Only CSV files are supported.")
+
+    contents = await file.read()
+
+    try:
+        buffer = StringIO(contents.decode("utf-8"))
+    except UnicodeDecodeError:
+        raise HTTPException(
+            status_code=400,
+            detail="There was an error decoding your file, please check the format and encoding.",
+        )
+
+    parser = csv.DictReader(buffer)
+
+    data = []
+
+    for row in parser:
+        data.append(row.copy())
+
+    return {"file": {"name": file.filename, "size": file.size}, "data": data}
---
+++ app/utils.py
@@ -0,0 +1,12 @@
+import csv
+import sys
+
+
+def increase_csv_field_size_limit():
+    max_int = sys.maxsize
+    while True:
+        try:
+            csv.field_size_limit(max_int)
+            break
+        except OverflowError:
+            max_int = int(max_int / 10)
